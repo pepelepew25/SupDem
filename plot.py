4a3a93395ba3dc4e15be9e27a21bb3e99cde13f9
Pepelepew25 <pepelepew@live.co.uk>
2019-04-08 15:53:39 +0100

    draft

--- plot.py
+++ plot.py
@@ -13,7 +13,7 @@ def plot_candlestick(df, ax=None, fmt="%Y-%m-%d"):
     ax.xaxis_date()
     ax.xaxis.set_major_formatter(mdates.DateFormatter(fmt))
     plt.xticks(rotation=45)
-    _ = candlestick_ohlc(ax, dat.values, width=.6, colorup='g', alpha =1)
+    _ = candlestick_ohlc(ax, dat.values, width=.6, colorup='g', alpha = 1)
     ax.set_xlabel(idx_name)
     ax.set_ylabel("OHLC")
     return ax
@@ -24,8 +24,8 @@ def plot_circle(ax,x,y):
     ax.add_artist(circle)
 
 
-def plot_rectangle(ax, startTime, endTime, low, high, color):
-    print("plot rectangle: startTime {} endTime {} low {} high {} color {}".format(startTime, endTime, low, high, color))
+def plot_rectangle(ax, startTime, endTime, low, high, color, text):
+    #print("plot rectangle: startTime {} endTime {} low {} high {} color {}".format(startTime, endTime, low, high, color))
     from matplotlib.patches import Rectangle
     #convert to matplotlib date representation
     start = mdates.date2num(startTime)
@@ -33,5 +33,10 @@ def plot_rectangle(ax, startTime, endTime, low, high, color):
     width = end - start
     height = high - low
 
-    rect = Rectangle((start, low), width, height, color=color)
+    rect = Rectangle((start, low), width, height, color=color, alpha=0.4)
+    rx, ry = rect.get_xy()
+    cx = rx + rect.get_width()/2.0
+    cy = ry + rect.get_height()/2.0
+    ax.annotate(text, (cx, cy), color='w', weight='bold', fontsize=6, ha='center', va='center')
+
     ax.add_patch(rect)
