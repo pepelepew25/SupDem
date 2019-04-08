fa26d79061c3518af71ca051a731d205695009fe
Pepelepew25 <pepelepew@live.co.uk>
2019-04-08 15:51:40 +0100

    added function to get MarketData using CCXT added code to resample the data and plot the zones on multiple timeframes

--- main.py
+++ main.py
@@ -2,24 +2,36 @@ from plot import *
 import pandas as pd
 import numpy as np
 from supply_demand_zones import *
+import sys
+#sys.path.append('../backtesting_bot/')
+import GetMarketData
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(message)s',
+                    #filename='backtesting.log',
+                    filemode='w')
 
-df = pd.read_pickle("gdax_onehour_30days.pkl")
+days_history = 300
+    #days_history = 100
+
+symbol = 'XBTUSD'
+#symbol = 'ETHUSD'
+
+df = GetMarketData.GetHistoricalMarketdata(symbol, days_history, 'False')
+if not len(df):
+    print("empty dataframe")
+    exit()
+
 df = df.dropna()
 
 df.index.name = 'Date'
-#df = df.resample('24H').agg({'Open': 'first',
-#                                 'High': 'max',
-#                                 'Low': 'min',
-#                                 'Close': 'last'})
-#df.insert(0, 'rownum', range(0,len(df)))
-
-#print(df)
 
 ax = plot_candlestick(df)
-
+#df = df.resample('4H').agg({'Open': 'first', 'High': 'max','Low': 'min','Close': 'last'})
+#SupDem(df, ax)
+df = df.resample('24H').agg({'Open': 'first', 'High': 'max','Low': 'min','Close': 'last'})
 SupDem(df, ax)
-
-
+df = df.resample('W').agg({'Open': 'first', 'High': 'max','Low': 'min','Close': 'last'})
+SupDem(df, ax)
 plt.tight_layout()
 # plt.savefig("candle.png")
 plt.show()
